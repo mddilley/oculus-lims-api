99c9d6ce2d7f30f2fd0f4506a086d9b0d8bcb4f3
Michael Dilley <mike.dilley@gmail.com>
2019-04-24 14:20:50 -0500

    Write seed data code in /db/seeds.rb and seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed batches
+
+creators = ["MDD", "ENE", "l337"]
+
+Batch.create([{
+  creator: creators[0],
+  date: Date.new(2019, 3, 12),
+  batch_cuid: "ascjok13nlknclsa"
+  },
+  {
+  creator: creators[1],
+  date: Date.new(2019, 2, 02),
+  batch_cuid: "fasdkjlk2n3lkfds"
+    },
+  {
+  creator: creators[2],
+  date: Date.new(2019, 4, 12),
+  batch_cuid: "2k3l1nmlkd2lk33k"
+    }])
+
+# Seed samples
+
+firstNames = ["Matt", "Dave", "Cynthia", "Liz", "Rebecca", "Tom", "Sally", "Steve"]
+lastNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+batch_ids = [1, 2, 3]
+
+id_array = (1..999).to_a
+age_array = (21..90).to_a
+
+20.times do |i|
+  Sample.create(
+    first_name: firstNames.sample,
+    last_name: lastNames.sample,
+    batch_id: batch_ids.sample,
+    age: age_array.sample,
+    sample_cuid: "#{lastNames.sample + id_array.sample.to_s + lastNames.sample + id_array.sample.to_s}"
+  )
+end

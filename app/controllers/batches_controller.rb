7d9f7f78fd7c9e01c94adc1807345642ea6d7bf9
Michael Dilley <mike.dilley@gmail.com>
2019-04-25 13:06:43 -0500

    Add BatchesController#create action and strong params to BatchesController

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -10,4 +10,15 @@ class BatchesController < ApplicationController
     render json: BatchSerializer.new(batch).serialized_json
   end
 
+  def create
+    # binding.pry
+    batch = Batch.create(batch_params)
+  end
+
+  private
+
+    def batch_params
+      params.require(:batch).permit(:creator, :date)
+    end
+
 end

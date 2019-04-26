223276afef026aa782079023c387fed42237afa7
Michael Dilley <mike.dilley@gmail.com>
2019-04-26 14:21:15 -0500

    Uncomment render json in SamplesController#index

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,9 +1,8 @@
 class SamplesController < ApplicationController
 
   def index
-    binding.pry
     samples = Sample.where(:batch_id === params[:batch_id])
-    # render json: SampleSerializer.new(samples).serialized_json
+    render json: SampleSerializer.new(samples).serialized_json
   end
 
 end

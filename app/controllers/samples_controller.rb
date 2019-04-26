d8db7b541721c384d66e29e8ff133af85f1a48f5
Michael Dilley <mike.dilley@gmail.com>
2019-04-26 14:06:57 -0500

    Add AR where query to populate batch samples in SamplesController#index

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,6 +2,8 @@ class SamplesController < ApplicationController
 
   def index
     binding.pry
+    samples = Sample.where(:batch_id === params[:batch_id])
+    # render json: SampleSerializer.new(samples).serialized_json
   end
 
 end

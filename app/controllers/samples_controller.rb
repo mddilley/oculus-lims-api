3665b6e342a19319e7dcf41ce32b6eef00979d72
Michael Dilley <mike.dilley@gmail.com>
2019-04-28 16:42:25 -0500

    Add SamplesController#create action and sample_params

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -5,8 +5,18 @@ class SamplesController < ApplicationController
     render json: SampleSerializer.new(samples).serialized_json
   end
 
+  def create
+    sample = Sample.create(sample_params)
+    render json: SampleSerializer.new(sample).serialized_json
+  end
+
   def destroy
     Sample.find(params[:id]).delete
   end
 
+  private
+
+    def sample_params
+      params.require(:sample).permit(:sample_cuid, :first_name, :last_name, :age, :batch_id)
+    end
 end

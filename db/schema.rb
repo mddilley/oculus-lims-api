9fc4a6639d4b38713e7cb4a95a38a80fb51279bb
Michael Dilley <mike.dilley@gmail.com>
2019-04-29 15:05:20 -0500

    Migrate and seed PG DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_24_171902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "batches", force: :cascade do |t|
     t.string "creator"
     t.datetime "date"
